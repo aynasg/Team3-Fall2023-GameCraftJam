71b512bd8f3b21964a3eef738b6cc1d06ca0aae3
InTheProcess <85518492+InTheProcess@users.noreply.github.com>
2023-09-15 22:12:37 -0600

    fixed-gravity-cost (#3)
    It was supposed to be 98 instead of 980.

--- scripts/world.gd
+++ scripts/world.gd
@@ -1,3 +1,3 @@
 # Since gravity on Earth is 9.8 mps, we set the game's gravity to it.
-# 2d games in Godot multiply this value by 100, hence 980
-const GRAVITY = 980
+# 2d games in Godot multiply this value by 10, hence 98
+const GRAVITY = 98
